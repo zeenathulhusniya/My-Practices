8fe004a26bac7f9acfb3d829c33c5aabdc81d47e
zeenathulhusniya <zeenathul.it@gmail.com>
2026-07-15 23:55:19 +0530

    Add initial HTML, CSS, and JavaScript files for DOM manipulation practice

---
+++ Front-End Development Practices/DOM/index.js
@@ -0,0 +1,9 @@
+
+document.querySelector("a");
+<a href="https://www.google.com">Google</a>
+document.querySelector("a").attribute;
+document.querySelector("a").getAttribute("href");
+"https://www.google.com"
+document.querySelector("a").setAttribute("href", "https://www.bing.com");
+
+
